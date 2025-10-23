5efed0eabdfa9ef75a8fe6fed381194c6f0afa9b
hallow.ween <sky608479@gmail.com>
2025-10-23 19:33:15 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function ShowImage() {
  const output = document.getElementById("ShowText");
 
-output.textContent = "test";
+output.textContent = "Your family could be just like this! Large families arent a 'curse' or anything bad, they are perfect and loving!";
 }
 
