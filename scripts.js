61cdbe4dac2f3d93ce500a986f001aa48dd0c35c
hallow.ween <sky608479@gmail.com>
2025-10-24 15:17:01 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function ShowImage() {
  const output = document.getElementById("ShowText");
 
-output.textContent = "Your family could be just like this! Large families arent a 'curse' or anything bad, they are perfect and loving! Some policies like our Assegno Unico Universale, which pays families money for every dependent child they have- which helps support any family!";
+output.textContent = "Due to policies like Assengo Unico Universale, which pays families money for every dependent child they have. This policy helps support not only the family but our country increase in population. Bigger families don't have to be a burden, they can be helpful.";
 }
 
