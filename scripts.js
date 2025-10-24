fa931fd434523b5220b59657925581327d2a3744
hallow.ween <sky608479@gmail.com>
2025-10-23 20:02:15 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function ShowImage() {
  const output = document.getElementById("ShowText");
 
-output.textContent = "Your family could be just like this! Large families arent a 'curse' or anything bad, they are perfect and loving!";
+output.textContent = "Your family could be just like this! Large families arent a 'curse' or anything bad, they are perfect and loving! Some policies like our Assegno Unico Universale, which pays families money for every dependent child they have- which helps support any family!";
 }
 
