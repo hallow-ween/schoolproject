25b5115898b96be9bde4bf679b8c2c19704911f2
hallow.ween <sky608479@gmail.com>
2025-10-27 09:42:25 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function ShowImage() {
  const output = document.getElementById("ShowText");
 
-output.textContent = "Due to policies like Assengo Unico Universale, which pays families money for every dependent child they have. This policy helps support not only the family but our country increase in population. Bigger families don't have to be a burden, they can be helpful.";
+output.textContent = "Due to policies like Assengo Unico Universale, which pays families money for every dependent child they have, Italy is becoming more family-friendly! This policy helps support not only the family but our country increase in population. Bigger families don't have to be a burden, they can be helpful and happy!";
 }
 
